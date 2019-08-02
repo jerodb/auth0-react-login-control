043905eb044975ebeed394225c00fdbf6888e1dc
jerodb <jero.db@live.com>
2019-08-02 19:35:32 -0300

    pass auth0 config and remove location as prop

--- src/components/Callback.js
+++ src/components/Callback.js
@@ -1,19 +1,21 @@
 import React, { useEffect } from 'react'
 import Auth0Manager from '../lib/Auth0Manager'
 
-function Callback({ location, onSetSession }) {
-  const AuthManager = new Auth0Manager()
+function Callback({ config, onSetSession = null }) {
+  const AuthManager = new Auth0Manager(config)
 
   useEffect(() => {
     const handleAuthentication = async () => {
-      if (/access_token|id_token|error/.test(location.hash)) {
+      if (/access_token|id_token|error/.test(document.location.hash)) {
         return AuthManager.handleAuthentication()
       }
       return null
     }
 
     handleAuthentication().then(session => {
-      if (onSetSession) onSetSession(session)
+      if (onSetSession) return onSetSession(session)
+
+      document.location.href='/'
     })
   })
 
